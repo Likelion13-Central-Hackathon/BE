b0695e15babbd8c410c517838d304a9a0171b802
khyaejin <olivia019182@naver.com>
2025-08-03 13:01:52 +0900

    🎨 Chore: 프롬프트 관련 주석 추가

--- src/main/java/com/likelion/server/infra/gpt/GptChatService.java
+++ src/main/java/com/likelion/server/infra/gpt/GptChatService.java
@@ -15,9 +15,11 @@ public class GptChatService {
         List<ChatMessage> messages = new ArrayList<>();
 
         // 시스템 프롬프트 추가
+        // ex. 너는 창업 지원 서류를 대신 작성해주는 AI야...
         messages.add(new ChatMessage("system", prompt));
 
         // 사용자 질문 추가
+        // ex. [문항]\n당신의 창업 아이템의 차별점은 무엇인가요?\n [아이디어 정보]\n20대 여성 맞춤형 생리용품 구독 서비스로...
         messages.add(new ChatMessage("user", question));
 
         GptChatRequest request = GptChatRequest.builder()
