625e6d1da76a815ab34f1bd331e05882ba6f4687
EunJi <univ217@naver.com>
2025-08-12 03:36:34 +0900

    :sparkles: Feat: Report 엔티티 생성

---
+++ src/main/java/com/likelion/server/domain/report/entity/Report.java
@@ -0,0 +1,60 @@
+package com.likelion.server.domain.report.entity;
+
+import com.likelion.server.domain.idea.entity.Idea;
+import com.likelion.server.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "reports")
+public class Report extends BaseEntity {
+    // PK
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 뉴스 N:1 리포트
+    @ManyToOne(fetch = FetchType.LAZY) // FK: ideas_id
+    @JoinColumn(name = "ideas_id", nullable = false)
+    private Idea idea;
+
+    // 분석각도
+    private Integer angle;
+
+    // 추천리서치방법
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String researchMethod;
+
+    // 강점
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String strength;
+
+    // 약점
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String weakness;
+
+    // 기회
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String opportunity;
+
+    // 위협
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String threat;
+
+    // 추천 계획(1~4차)
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String step1;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String step2;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String step3;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String step4;
+
+    // 기대효과
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String expectedEffect;
+}
