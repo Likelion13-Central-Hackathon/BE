17d5c60315c5c8ee651fa927313f2af953c68cd9
khyaejin <olivia019182@naver.com>
2025-08-11 21:41:44 +0900

    ⚡️ Perf: 창업 아이디어 생성 관련 예외처리 정교화

--- src/main/java/com/likelion/server/domain/idea/web/dto/CreateIdeaRequest.java
+++ src/main/java/com/likelion/server/domain/idea/web/dto/CreateIdeaRequest.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 // 창업 아이디어 생성 Request Dto
 public record CreateIdeaRequest(
+        @Min(value = 1, message = "age는 1 이상이어야 합니다.")
         int age,
 
         @NotBlank(message = "addressCity는 필수 입력 값입니다.")
@@ -16,11 +17,10 @@ public record CreateIdeaRequest(
         String addressDistrict,
 
         // 대학 재학여부
-        @NotBlank(message = "isEnrolled는 필수 입력 값입니다.")
         boolean isEnrolled,
 
-        String university, // isEnrolled=false -> null
-        AcademicStatus academicStatus, // isEnrolled=false -> null
+        String university,              // isEnrolled=false -> null
+        AcademicStatus academicStatus,  // isEnrolled=false -> null
 
         // 관심분야
         @NotBlank(message = "interestArea는 필수 입력 값입니다.")
@@ -28,6 +28,7 @@ public record CreateIdeaRequest(
 
         // 필요한 자원들(자원이름, 레벨)
         @NotNull(message = "supportNeeds는 필수입니다.")
+        @Size(min = 8, max = 8, message = "supportNeeds는 8개의 항목이 모두 포함되어야 합니다.")
         Map<NeedType, Level> supportNeeds,
 
         // 업력
@@ -52,6 +53,7 @@ public record CreateIdeaRequest(
 
         // 활용가능 자원들
         @NotNull(message = "resources는 필수입니다.")
+        @Size(min = 6, max = 6, message = "resources는 6개의 항목이 모두 포함되어야 합니다.")
         Map<ResourceType, Level> resources // 활용 가능 자원들(자원이름, 레벨)
 ) {
 }
