7da2dcf64d98fb7ecaee6ca31c29f842f5ca05c5
EunJi <univ217@naver.com>
2025-08-12 22:03:49 +0900

    :sparkles: Feat: 리포트 상세 조회 로직 구현

--- src/main/java/com/likelion/server/domain/report/service/ReportQueryService.java
+++ src/main/java/com/likelion/server/domain/report/service/ReportQueryService.java
@@ -5,4 +5,6 @@ import com.likelion.server.domain.report.web.dto.LatestReportDetailResponse;
 
 public interface ReportQueryService {
     LatestReportDetailResponse getLatestReport(LatestReportDetailRequest request);
+
+    LatestReportDetailResponse getById(Long reportId);
 }
