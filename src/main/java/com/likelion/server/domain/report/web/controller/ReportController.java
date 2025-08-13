fabf3fe8c11c9dc786ddd16639f0b4a0a1af49b5
khyaejin <olivia019182@naver.com>
2025-08-13 22:36:08 +0900

    ✨ Feat: Report 생성 - Controller, Dto 로직 구현

--- src/main/java/com/likelion/server/domain/report/web/controller/ReportController.java
+++ src/main/java/com/likelion/server/domain/report/web/controller/ReportController.java
@@ -3,6 +3,7 @@ package com.likelion.server.domain.report.web.controller;
 
 import com.likelion.server.domain.report.service.ReportService;
 import com.likelion.server.domain.report.web.dto.LatestReportDetailRequest;
+import com.likelion.server.domain.report.web.dto.ReportCreateResponse;
 import com.likelion.server.domain.report.web.dto.ReportDetailResponse;
 import com.likelion.server.global.response.SuccessResponse;
 import jakarta.validation.Valid;
@@ -11,26 +12,32 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/reports")
+@RequestMapping()
 public class ReportController {
 
-    private final ReportService reportQueryService;
+    private final ReportService reportService;
 
     // 레포트 생성
-    @PostMapping()
+    @PostMapping("/ideas/{ideaId}/reports")
+    public SuccessResponse<ReportCreateResponse> createReport(
+            @RequestParam("ideaId") Long ideaId
+    ) {
+        return null;
+    }
+
 
     // 최근 리포트 상세 조회
-    @GetMapping("/latest")
+    @GetMapping("/reports/latest")
     public SuccessResponse<ReportDetailResponse> getLatestReport(
             @RequestBody @Valid LatestReportDetailRequest request
     ) {
-        ReportDetailResponse data = reportQueryService.getLatestReport(request);
+        ReportDetailResponse data = reportService.getLatestReport(request);
         return SuccessResponse.ok(data);
     }
 
     // ID 기반 상세 조회
-    @GetMapping("/{reportId}")
+    @GetMapping("/reports/{reportId}")
     public SuccessResponse<ReportDetailResponse> getById(@PathVariable Long reportId) {
-        return SuccessResponse.ok(reportQueryService.getById(reportId));
+        return SuccessResponse.ok(reportService.getById(reportId));
     }
 }
