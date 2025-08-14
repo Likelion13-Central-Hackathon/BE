f96efa4ec8e49f3c611adc6ddf37c258150cb40d
khyaejin <olivia019182@naver.com>
2025-08-14 23:06:45 +0900

    ✨ Feat: StartupSupport 엔티티 필요한 필드 추가

--- src/main/java/com/likelion/server/domain/startupSupport/entity/StartupSupport.java
+++ src/main/java/com/likelion/server/domain/startupSupport/entity/StartupSupport.java
@@ -1,5 +1,6 @@
 package com.likelion.server.domain.startupSupport.entity;
 
+import com.likelion.server.domain.startupSupport.entity.enums.BusinessDuration;
 import com.likelion.server.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -21,9 +22,69 @@ public class StartupSupport extends BaseEntity {
     @Column(nullable = false)
     private String title;
 
+    // 지원분야
+    private String supportArea;
+
+    // 지역
+    private String region;
+
+    // 업력 대상
+    private BusinessDuration businessDuration;
+
+    // 주관기관명
+    private String agency;
+
+    // 나이 제한
+    private String targetAge;
+
+    // 지원 대상
+    private String target;
+
+    // 연락처
+    private String contact;
+
+    // 상세링크
+    private String link;
+
+    // 핵심 내용 요약
+    @Lob
+    private String coreContent;
+
     // 모집 시작일
     private LocalDate startDate;
 
     // 모집 종료일
     private LocalDate endDate;
+
+    // 신청 방법
+    private String applyMethod;
+
+    // 신청 절차
+    @Lob
+    private String applyProcedure;
+
+    // 평가 방법
+    @Lob
+    private String evaluationMethod;
+
+    // 제출 서류
+    @Lob
+    private String requiredDocuments;
+
+    // 지원 예산 및 규모
+    @Lob
+    private String budget;
+
+    // 지원 내용
+    @Lob
+    private String supportDetails;
+
+    // 사업 특징
+    @Lob
+    private String businessFeature;
+
+    // 사업 소개 정보
+    @Lob
+    private String businessIntro;
+
 }
