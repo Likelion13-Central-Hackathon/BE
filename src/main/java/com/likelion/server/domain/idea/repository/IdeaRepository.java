6db2feb6cdd02fe5e2fe37d5cc4ba396f24488c7
EunJi <univ217@naver.com>
2025-08-14 23:25:26 +0900

    :sparkles: Feat: 아이디어 조회 메서드 추가

--- src/main/java/com/likelion/server/domain/idea/repository/IdeaRepository.java
+++ src/main/java/com/likelion/server/domain/idea/repository/IdeaRepository.java
@@ -2,8 +2,13 @@ package com.likelion.server.domain.idea.repository;
 
 import com.likelion.server.domain.idea.entity.Idea;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IdeaRepository extends JpaRepository<Idea, Long> {
+    @Query("select i from Idea i where i.receiveNotification = true and i.user is not null")
+    List<Idea> findAllSubscribed();
 }
