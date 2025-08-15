41f2299a93ea9cd0f395e95be6148db377a5631b
khyaejin <olivia019182@naver.com>
2025-08-15 14:36:30 +0900

    ✨ Faet: BaseEntity에 yyyy.mm.dd 형식으로 바꾸는 메서드 추가

--- src/main/java/com/likelion/server/global/entity/BaseEntity.java
+++ src/main/java/com/likelion/server/global/entity/BaseEntity.java
@@ -9,11 +9,14 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+
     @CreatedDate
     @Column(name = "created_at")
     private LocalDateTime createdAt;
@@ -21,4 +24,9 @@ public abstract class BaseEntity {
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    //LocalDateTime -> yyyy.MM.dd
+    protected static String formatYmd(LocalDateTime dt) {
+        return (dt == null) ? null : dt.toLocalDate().format(formatter);
+    }
 }
