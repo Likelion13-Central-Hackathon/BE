945585819c308dec89c89e3cb1ad9c816711e853
khyaejin <olivia019182@naver.com>
2025-08-15 22:56:12 +0900

    🎨 Fix: Fix: k-startup에서 제공해주는 형식에 맞게 업력 매핑 로직 수정

--- src/main/java/com/likelion/server/domain/startupSupport/mapper/BusinessDurationMapper.java
+++ src/main/java/com/likelion/server/domain/startupSupport/mapper/BusinessDurationMapper.java
@@ -15,39 +15,39 @@ public final class BusinessDurationMapper {
             entry("예비 창업자", BusinessDuration.PRE_ENTREPRENEUR),
             entry("예비", BusinessDuration.PRE_ENTREPRENEUR),
 
-            entry("1년 미만", BusinessDuration.UNDER_1_YEAR),
-            entry("창업 1년 미만", BusinessDuration.UNDER_1_YEAR),
-            entry("1년 이하", BusinessDuration.UNDER_1_YEAR),
-            entry("창업 1년 이하", BusinessDuration.UNDER_1_YEAR),
+            entry("1년미만", BusinessDuration.UNDER_1_YEAR),
+            entry("창업 1년미만", BusinessDuration.UNDER_1_YEAR),
+            entry("1년이하", BusinessDuration.UNDER_1_YEAR),
+            entry("창업 1년이하", BusinessDuration.UNDER_1_YEAR),
 
-            entry("2년 미만", BusinessDuration.UNDER_2_YEARS),
-            entry("창업 2년 미만", BusinessDuration.UNDER_2_YEARS),
-            entry("2년 이하", BusinessDuration.UNDER_2_YEARS),
+            entry("2년미만", BusinessDuration.UNDER_2_YEARS),
+            entry("창업 2년미만", BusinessDuration.UNDER_2_YEARS),
+            entry("2년이하", BusinessDuration.UNDER_2_YEARS),
 
-            entry("3년 미만", BusinessDuration.UNDER_3_YEARS),
-            entry("창업 3년 미만", BusinessDuration.UNDER_3_YEARS),
-            entry("3년 이하", BusinessDuration.UNDER_3_YEARS),
+            entry("3년미만", BusinessDuration.UNDER_3_YEARS),
+            entry("창업 3년미만", BusinessDuration.UNDER_3_YEARS),
+            entry("3년이하", BusinessDuration.UNDER_3_YEARS),
 
-            entry("5년 미만", BusinessDuration.UNDER_5_YEARS),
-            entry("창업 5년 미만", BusinessDuration.UNDER_5_YEARS),
-            entry("5년 이하", BusinessDuration.UNDER_5_YEARS),
+            entry("5년미만", BusinessDuration.UNDER_5_YEARS),
+            entry("창업 5년미만", BusinessDuration.UNDER_5_YEARS),
+            entry("5년이하", BusinessDuration.UNDER_5_YEARS),
 
-            entry("7년 미만", BusinessDuration.UNDER_7_YEARS),
-            entry("7년 이하", BusinessDuration.UNDER_7_YEARS),
+            entry("7년미만", BusinessDuration.UNDER_7_YEARS),
+            entry("7년이하", BusinessDuration.UNDER_7_YEARS),
 
-            entry("10년 미만", BusinessDuration.UNDER_10_YEARS),
-            entry("10년 이하", BusinessDuration.UNDER_10_YEARS)
+            entry("10년미만", BusinessDuration.UNDER_10_YEARS),
+            entry("10년이하", BusinessDuration.UNDER_10_YEARS)
     );
 
     // enum -> String
     private static final Map<BusinessDuration, String> ENUM_TO_KOR = Map.ofEntries(
             entry(BusinessDuration.PRE_ENTREPRENEUR, "예비창업자"),
-            entry(BusinessDuration.UNDER_1_YEAR, "1년 미만"),
-            entry(BusinessDuration.UNDER_2_YEARS, "2년 미만"),
-            entry(BusinessDuration.UNDER_3_YEARS, "3년 미만"),
-            entry(BusinessDuration.UNDER_5_YEARS, "5년 미만"),
-            entry(BusinessDuration.UNDER_7_YEARS, "7년 미만"),
-            entry(BusinessDuration.UNDER_10_YEARS, "10년 미만")
+            entry(BusinessDuration.UNDER_1_YEAR, "1년미만"),
+            entry(BusinessDuration.UNDER_2_YEARS, "2년미만"),
+            entry(BusinessDuration.UNDER_3_YEARS, "3년미만"),
+            entry(BusinessDuration.UNDER_5_YEARS, "5년미만"),
+            entry(BusinessDuration.UNDER_7_YEARS, "7년미만"),
+            entry(BusinessDuration.UNDER_10_YEARS, "10년미만")
     );
 
     // String -> Enum
