7b69acfd45c5e22f9944891bff107edf89cd8360
김혜진 <olivia019182@naver.com>
2025-08-20 13:30:07 +0900

    ♻️Refactor: RecommendedStartupSupportDetailResponse 수정
    상세 조회때 사용한 dto를 조회에 사용할 수 있도록 수정 및 날짜 포맷 수정

--- src/main/java/com/likelion/server/domain/recommendedStartupSupport/web/dto/RecommendedStartupSupportDetailResponse.java
+++ src/main/java/com/likelion/server/domain/recommendedStartupSupport/web/dto/RecommendedStartupSupportDetailResponse.java
@@ -5,14 +5,15 @@ import com.likelion.server.domain.startupSupport.entity.StartupSupport;
 import com.likelion.server.domain.startupSupport.mapper.RegionMapper;
 
 public record RecommendedStartupSupportDetailResponse(
+        int suitability,              // 적합도
         String supportArea,           // 지원 분야
+        String agency,                // 주관기관
         String title,                 // 제목
         String link,                  // 본문 접속 링크 (K-Startup)
-        String startDate,             // YYYY.MM.DD
-        String endDate,               // YYYY.MM.DD
+        String startDate,             // 시작일 (mm.dd)
+        String endDate,               // 종료일 (mm.dd)
         String region,                // 지역
         String businessDuration,      // 업력
-        String agency,                // 주관기관
         String targetAge,             // 나이 범위
         String target,                // 지원 대상
         String contact,               // 연락처
@@ -20,28 +21,27 @@ public record RecommendedStartupSupportDetailResponse(
         String supportDetails,        // 지원 내용(상세)
         String guidanceUrl,           // 안내 페이지 URL
         Boolean isRecruiting,         // 모집 여부
-        int suitability,              // 적합도
         String reason                 // 추천 사유
 ) {
     public static RecommendedStartupSupportDetailResponse of(StartupSupport s, RecommendedStartupSupport r) {
         return new RecommendedStartupSupportDetailResponse(
-                s.getSupportArea(),
-                s.getTitle(),
-                s.getLink(),
-                s.formatYmd(s.getStartDate()),
-                s.formatYmd(s.getEndDate()),
-                RegionMapper.toString(s.getRegion()),
-                s.getBusinessDuration(),
-                s.getAgency(),
-                s.getTargetAge(),
-                s.getTarget(),
-                s.getContact(),
-                s.getApplyMethod(),
-                s.getSupportDetails(),
-                s.getGuidanceUrl(),
-                s.getIsRecruiting(),
-                r.getSuitability(),
-                r.getReason()
+                r.getSuitability(),              // 적합도
+                s.getSupportArea(),              // 지원 분야
+                s.getAgency(),                   // 주관기관
+                s.getTitle(),                    // 제목
+                s.getLink(),                     // 본문 접속 링크
+                s.formatMd(s.getStartDate()),    // 시작일
+                s.formatMd(s.getEndDate()),      // 종료일
+                RegionMapper.toString(s.getRegion()), // 지역
+                s.getBusinessDuration(),         // 업력
+                s.getTargetAge(),                // 나이 범위
+                s.getTarget(),                   // 지원 대상
+                s.getContact(),                  // 연락처
+                s.getApplyMethod(),              // 신청 방법
+                s.getSupportDetails(),           // 지원 내용
+                s.getGuidanceUrl(),              // 안내 페이지
+                s.getIsRecruiting(),             // 모집 여부
+                r.getReason()                    // 추천 사유
         );
     }
 }
