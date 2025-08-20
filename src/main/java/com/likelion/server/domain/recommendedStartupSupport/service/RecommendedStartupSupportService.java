673609c90749a0044fc7bb6405824bd4975922dc
김혜진 <olivia019182@naver.com>
2025-08-20 13:50:27 +0900

    ♻️Refactor: 추천 지원사업 조회 service 수정

--- src/main/java/com/likelion/server/domain/recommendedStartupSupport/service/RecommendedStartupSupportService.java
+++ src/main/java/com/likelion/server/domain/recommendedStartupSupport/service/RecommendedStartupSupportService.java
@@ -1,13 +1,12 @@
 package com.likelion.server.domain.recommendedStartupSupport.service;
 
 import com.likelion.server.domain.recommendedStartupSupport.web.dto.RecommendedStartupSupportDetailResponse;
-import com.likelion.server.domain.recommendedStartupSupport.web.dto.RecommendedStartupSupportSummaryResponse;
 
 import java.util.List;
 
 public interface RecommendedStartupSupportService {
     // 레포트 기반 추천 창업 지원 사업 목록 조회
-    public List<RecommendedStartupSupportSummaryResponse> getByReportId(Long reportId);
+    public List<RecommendedStartupSupportDetailResponse> getByReportId(Long reportId);
     // 추천 창업 지원 사업 상세 조회
     public RecommendedStartupSupportDetailResponse getById(Long recommendedStartupSupportId);
 }
