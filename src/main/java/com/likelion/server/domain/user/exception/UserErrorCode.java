cb39ace2abec0a64ae430cc59ab5661da96f2324
김혜진 <olivia019182@naver.com>
2025-08-22 15:10:59 +0900

    🎨Chore: UserErrorCode 이름 변경

--- src/main/java/com/likelion/server/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/likelion/server/domain/user/exception/UserErrorCode.java
@@ -9,7 +9,7 @@ import static com.likelion.server.global.constant.StaticValue.NOT_FOUND;
 @Getter
 @AllArgsConstructor
 public enum UserErrorCode implements BaseResponseCode {
-    USER_404_NOT_FOUND("USER_404_NOT_FOUND", NOT_FOUND, "해당 ID의 회원을 찾을 수 없습니다.");
+    USER_404_NOT_FOUND_BY_IDEA("USER_404_NOT_FOUND_BY_IDEA", NOT_FOUND, "해당 아이디어의 회원을 찾을 수 없습니다.");
 
     private final String code;
     private final int httpStatus;
