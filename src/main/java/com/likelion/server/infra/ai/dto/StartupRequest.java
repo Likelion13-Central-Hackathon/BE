6e64b6c64e40b40513f9f89a7b4292a815935c4c
khyaejin <olivia019182@naver.com>
2025-08-23 02:21:56 +0900

    ♻️ Refactor: StartupRequest에 JsonProperty 제거

--- src/main/java/com/likelion/server/infra/ai/dto/StartupRequest.java
+++ src/main/java/com/likelion/server/infra/ai/dto/StartupRequest.java
@@ -1,9 +1,7 @@
 package com.likelion.server.infra.ai.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record StartupRequest (
-        @JsonProperty("idea_title") String ideaTitle,
-        @JsonProperty("idea_description") String ideaDescription
+        String ideaTitle,
+        String ideaDescription
 ){
 }
