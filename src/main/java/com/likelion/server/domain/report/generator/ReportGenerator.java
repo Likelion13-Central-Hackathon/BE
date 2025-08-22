2dea8346074cd0c1bbc8a9f96033b90d7ddc1503
김혜진 <olivia019182@naver.com>
2025-08-22 16:30:06 +0900

    🐛Fix: ReportGenerator 매개변수에 title 추가

--- src/main/java/com/likelion/server/domain/report/generator/ReportGenerator.java
+++ src/main/java/com/likelion/server/domain/report/generator/ReportGenerator.java
@@ -4,5 +4,5 @@ import com.likelion.server.domain.idea.entity.Idea;
 import com.likelion.server.domain.report.entity.Report;
 
 public interface ReportGenerator {
-    Report generate(Idea idea,  String ideaText);
+    Report generate(Idea idea,  String ideaText, String title);
 }
